7659c0a784de7be9fa339004d314dca4a31ef78c
Abby Mapes <acm103@duke.edu>
2021-04-06 12:46:49 -0400

    Test push

--- blueBook/server/index.js
+++ blueBook/server/index.js
@@ -119,6 +119,3 @@ app.get('/bluebook/reactions.json',
       res.json(reactions);
     }
 );
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('App listening on port ' + PORT));
