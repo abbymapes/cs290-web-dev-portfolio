2621eecb961892212d27ded71c6ca546b08cd774
Abby Mapes <acm103@duke.edu>
2021-04-06 12:04:48 -0400

    Fix port for heroku

--- blueBook/server/index.js
+++ blueBook/server/index.js
@@ -117,6 +117,5 @@ app.get('/bluebook/reactions.json',
     }
 );
 
-/*
-const PORT = 3030;
-app.listen(PORT, () => console.log('App listening on port ' + PORT));*/
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('App listening on port ' + PORT));
